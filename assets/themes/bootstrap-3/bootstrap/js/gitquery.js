e6ebbe0ff9f7ff434e20b78ed80442224f033d4d
AlbertoED <alberto.esteban.diaz@alumnos.upm.es>
2016-01-28 22:14:36 +0100

    Modificado aspecto listado repositorios API

--- assets/themes/bootstrap-3/bootstrap/js/gitquery.js
+++ assets/themes/bootstrap-3/bootstrap/js/gitquery.js
@@ -1,66 +1,64 @@
-jQuery.gitUser = function (username, callback) {
-        jQuery.getJSON('https://api.github.com/users/' + username + '/repos?per_page=100&callback=?', callback) //Change per_page according to your need.
-    }
+    // Para referenciar a nuestra app de FireBase
+    var myDataRef = new Firebase('https://shining-torch-549.firebaseio.com/');
+    var cuentaGit = "albertoed"; 
 
-    jQuery.fn.loadRepositories = function (username) {
-        this.html("<h2 style=\"color:#FFF;\">Hold on tight, digging out " + username + "'s repositories...</h2><br>");
+    jQuery.gitUser = function (callback) {
+        jQuery.getJSON('https://api.github.com/users/' + cuentaGit + '/repos?per_page=1000&&access_token=f0b6a501c4e4fa1ea5cb289b26a293dccdde879d&callback=?', callback) //Change per_page according to your need.
+    };
 
+    jQuery.gitReadme = function(repositorio,callback) {
+            jQuery.getJSON('https://api.github.com/repos/' + cuentaGit + '/' + repositorio + '/readme?access_token=f0b6a501c4e4fa1ea5cb289b26a293dccdde879d&callback=?', callback)
+            //SE USARA PARA LA INFO COMPLETA DE CADA PROYECTO
+                                    //Realizamos una petición GET del readme del proyecto
+                    $.gitReadme(this.name, function (data) {
+                var file = data.data;
+
+                console.log(decodeURIComponent(escape(window.atob(file.content))));
+        });
+    };
+
+    jQuery.fn.cargaRepositoriosGithub = function () {
         var target = this;
-        $.gitUser(username, function (data) {
+        $.gitUser(function (data) {
         var repos = data.data; // JSON Parsing
-        //alert(repos.length); Only for checking how many items are returned.
-        sortByForks(repos); //Sorting by forks. You can customize it according to your needs.
-        var list = $('<dl/>');
-        target.empty().append(list);
-        $(repos).each(function () {
-            checkfork = this.fork;
-            if ((this.name != (username.toLowerCase() + '.github.io')) && (checkfork != true)) { //Check for username.github.com repo and for forked projects
-                list.append('<dt><a style="font-size:20px;" href="' + (this.homepage ? this.homepage : this.html_url) + '">' + this.name + '</a><em> - ' + (this.language ? ('(' + this.language + ')') : '') + '</em><br>Forks: ' + this.forks + ' | Watchers: ' + this.watchers + '</dt>');
-                //Similarly fetch everything else you need.
-            }
-        });
-        });
+                /*var reposRef = myDataRef.child("repos");
+                reposRef.set(repos);*/
+                console.log(repos.length); //Only for checking how many items are returned.
+                //sortByForks(repos); //Sorting by forks. You can customize it according to your needs.
+                //var list = $('<dl/>');
+                //target.empty().append(list);
+                var node = document.getElementById('display-projects');
+                $(repos).each(function () {
+                    checkfork = this.fork;
+                    if (this.name != (cuentaGit.toLowerCase() + '.github.io')){ //Check for cuentaGit.github.com repo and for forked projects
+                        //list.append('<dt><a style="font-size:20px;" href="' + (this.homepage ? this.homepage : this.html_url) + '">' + this.name + '</a><em> - ' + (this.language ? ('(' + this.language + ')') : '') + '</em><br>Forks: ' + this.forks + ' | Watchers: ' + this.watchers + '</dt>');
 
-        function sortByForks(repos) {
+                        // Anidamos los contenedores para cada proyecto que nos devuelva el GET:
+
+                       node.innerHTML += '<div class="panel panel-primary"><div class="panel-heading" style="background-color: #0683AD;background-image: none;"><p class="titleReposAdmin">' + this.name + 
+                       '</p></div><div class="panel-body"><div class="row"><div class="col-md-2"><p><b>Autor: </b>'+ this.owner.login + '</p></div>' + 
+                       '<div class="col-md-4"><p><b>Fecha Creación: </b>'+ this.created_at + '</p></div>' +
+                       '<div class="col-md-5"><p><b>Categoría: </b><select><option hidden >Seleccione una categoría</option><option>Agentes y Simulación Social</option><option>Big Data y Aprendizaje Automático</option><option>NLP y Análisis de Sentimientos</option><option>La Web de Datos y Tecnologías Semánticas</option><option>Ingeniería Web y de servicios</option><option>Otros</option></select></p></div>' +
+                       '<div class="col-md-1"><input data-toggle="toggle" type="checkbox" id="toggle' + this.id + '"></div></div></div></div>';
+       
+                    }
+                });
+        });
+    };
+
+    function sortByForks(repos) {
         repos.sort(function (a, b) {
             return b.forks - a.forks; //Descending order for number of forks based sorting.
         });
-        }
     };
 
     function funcionPost() {
-        /*
-                         var xhttp = new XMLHttpRequest();
-                        xhttp.open("POST", "php/testapi.php", true);
-                        xhttp.send();
-                        */
-
-                       /* 
-                var data = {'action': 'follow', 'userid': '11239528343'};
-                $.post("testapi.php",
-                    data,
-                    function(response,status){
-                    alert("Status : " + status);
+        myDataRef.on("value", function(snapshot) {
+                console.log(snapshot.val());
+                }, function (errorObject) {
+                        console.log("The read failed: " + errorObject.code);
                 });
-*/
-
-        
-        $.ajax({
-            url: 'http://albertoed.github.io/testapi.php',
-            type: 'POST',
-            data: {'action': 'follow', 'userid': '11239528343'},
-            success:function(result, status) {
-                        if(result == "ok") {
-                            console.log("Status : " + status);
-                            console.log("Result : " + result);
-                        }
-                    },
-            error: function(xhr, desc, err) {
-                        console.log(xhr);
-                        console.log("Details: " + desc + "\nError:" + err);
-                    }
-        }); // end ajax call   
-
+           /* var reposRef = myDataRef.child("repos");
+            reposRef.set*/
 
     };
-
--- testapi.php
+++ testapi.php
@@ -1,13 +1,6 @@
 <?php
-echo "hola";
+
+echo "HOLA2";
 die();
 
-if($_POST['action'] == "follow") {
-  /**
-   * we can pass any action like block, follow, unfollow, send PM....
-   * if we get a 'follow' action then we could take the user ID and create a SQL command
-   * but with no database, we can simply assume the follow action has been completed and return 'ok'
-  **/
-  echo "ok";
-}
 ?>
