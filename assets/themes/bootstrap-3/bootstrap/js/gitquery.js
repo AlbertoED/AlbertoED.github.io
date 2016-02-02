9f302138cb5018d436154a07b5655cff4ff82775
AlbertoED <alberto.esteban.diaz@alumnos.upm.es>
2016-02-02 19:38:53 +0100

    Esquema index

--- assets/themes/bootstrap-3/bootstrap/js/gitquery.js
+++ assets/themes/bootstrap-3/bootstrap/js/gitquery.js
@@ -2,9 +2,16 @@
     var myDataRef = new Firebase('https://shining-torch-549.firebaseio.com/');
     var cuentaGit = "albertoed"; 
     var tokenGit;
+    var currentPage = 1;
+    var loadingProjects = true;
+    var stopLoadingProjects = false;
 
     //eN CASO DE FALLO QUE MUESTRE UN MENSAJE DE ERROR EN LA CARGA
     jQuery.fn.cargaRepositoriosGithub = function () {
+        if (loadingProjects || stopLoadingProjects){
+            return;
+        }
+        loadingProjects = true;
         var target = this;
         //Recuperamos el Token para poder utilizar GET en el github con los permisos de visualizar repositorios privados y públicos
         console.log(tokenGit);
@@ -13,29 +20,36 @@
                 /*var reposRef = myDataRef.child("repos");
                 reposRef.set(repos);*/
                 console.log(repos.length); //Only for checking how many items are returned.
+                if (repos.length == 0){
+                    $('#load-more').remove();
+                    stopLoadingProjects = true;
+                    return;
+                }
                 //sortByForks(repos); //Sorting by forks. You can customize it according to your needs.
                 //var list = $('<dl/>');
                 //target.empty().append(list);
-                var node = document.getElementById('display-projects');
+                var node = $('#display-projects');
                 $(repos).each(function () {
                     checkfork = this.fork;
                     if (this.name != (cuentaGit.toLowerCase() + '.github.io')){ //Check for cuentaGit.github.com repo and for forked projects
                         //Las columnas deben sumar 12
                         // Anidamos los contenedores para cada proyecto que nos devuelva el GET:
-                       node.innerHTML += '<div class="panel panel-primary"><div class="panel-heading" style="background-color: #0683AD;background-image: none;"><p class="titleReposAdmin">' + this.name + 
-                       '</p></div><div class="panel-body"><div class="row"><div class="col-md-2"><p><b>Autor: </b>'+ this.owner.login + '</p></div>' + 
+                       node.append('<div class="panel panel-primary"><div class="panel-heading" style="background-color: #0683AD;background-image: none;"><p class="titleReposAdmin"><a href="' + this.html_url + '" target="_blank">' + this.name + '</a></p></div>' +
+                       '<div class="panel-body"><div class="row"><div class="col-md-3"><p><b>Autor: </b>'+ this.owner.login + '</p><p><b>ID repositorio: </b>' + this.id + '</p></div>' + 
                        '<div class="col-md-3"><p><b>Fecha Creación: </b>'+ this.created_at.substring(0,10) + '</p></div>' +
-                       '<div class="col-md-5"><p><b>Categoría: </b><select><option hidden >Seleccione una categoría</option><option>Agentes y Simulación Social</option><option>Big Data y Aprendizaje Automático</option><option>NLP y Análisis de Sentimientos</option><option>La Web de Datos y Tecnologías Semánticas</option><option>Ingeniería Web y de servicios</option><option>Otros</option></select></p></div>' +
-                       '<div class="col-md-2"><p><b>¿Mostrar?: </b><input data-toggle="toggle" type="checkbox"  id="toggle' + this.id + '"></p></div></div></div></div>';
+                       '<div class="col-md-4"><p><b></b><select><option hidden >Seleccione una categoría</option><option>Agentes y Simulación Social</option><option>Big Data y Aprendizaje Automático</option><option>NLP y Análisis de Sentimientos</option><option>La Web de Datos y Tecnologías Semánticas</option><option>Ingeniería Web y de servicios</option><option>Otros</option></select></p></div>' +
+                       '<div class="col-md-2"><p><b>¿Mostrar?: </b><input data-toggle="toggle" type="checkbox"  id="toggle' + this.id + '"></p></div></div></div></div>');
                     }
                 });
-                //CUANDO TERMINE DE CARGAR LOS REPOSITORIOS EN BOTON DE GUARDAR SERÁ VISIBLE
-                node.innerHTML += '<button id="btnGuardarRepositorios" type="button" class="btn btn-success btn-lg izda">Guardar</button>';
+                //CUANDO TERMINE DE CARGAR LOS REPOSITORIOS EL BOTON DE GUARDAR SERÁ VISIBLE
+                $('#btnGuardarRepositorios').show();
                 //IMPORTANTE: esta linea transforma todos los checkboxes que hemos añadido al html en los toggles
                 $('input[type="checkbox"]').bootstrapToggle({
                         on: 'Sí',
                         off: 'No'
                 });
+                //...
+                loadingProjects = false;
         });
     };
 
@@ -47,6 +61,7 @@
                 $(tokens).each(function () {
                     if (this.user == cuentaGit){
                         tokenGit = this.token;
+                        loadingProjects = false;
                         $("#display-projects").cargaRepositoriosGithub();
                         //return tokenGit;
                     }
@@ -55,14 +70,14 @@
     };
 
     function guardarSeleccion(){
-
-        $(".toggle").not(".off").children("input").attr("id").each(function(){
-            console.log(this.id);      
+        $(".toggle").not(".off").children("input").each(function(){
+             console.log($(this).attr('id'));                 
         });
+        $('#confirmar-guardar').modal('hide');
     };
 
     jQuery.gitUser = function (callback) {
-        jQuery.getJSON('https://api.github.com/users/' + cuentaGit + '/repos?per_page=1000&access_token=' + tokenGit + '&callback=?', callback); //Change per_page according to your need.
+        jQuery.getJSON('https://api.github.com/users/' + cuentaGit + '/repos?per_page=1&access_token=' + tokenGit + '&page=' + currentPage + '&callback=?', callback); //Change per_page according to your need.
         console.log('https://api.github.com/users/' + cuentaGit + '/repos?per_page=1000&access_token=' + tokenGit + '&callback=?');
     };
 
@@ -70,7 +85,7 @@
             jQuery.getJSON('https://api.github.com/repos/' + cuentaGit + '/' + repositorio + '/readme?access_token=f0b6a501c4e4fa1ea5cb289b26a293dccdde879d&callback=?', callback)
             //SE USARA PARA LA INFO COMPLETA DE CADA PROYECTO
                      //Realizamos una petición GET del readme del proyecto
-                    $.gitReadme(this.name, function (data) {
+                $.gitReadme(this.name, function (data) {
                 var file = data.data;
 
                 console.log(decodeURIComponent(escape(window.atob(file.content))));
@@ -92,3 +107,39 @@
            /* var reposRef = myDataRef.child("repos");
             reposRef.set*/
     };
+
+    //FUNCIÓN PARA LLAMAR A MÁS PROYECTOS AL HACER SCROLL
+    function loadMore() {
+        console.log("entra click");
+        currentPage++;
+        $("#display-projects").cargaRepositoriosGithub();
+    };
+
+    // CODIGO QUE SE EJECUTA CUANDO YA SE HA CARGADO LA PÁGINA
+    $(window).load(function(){
+        getTokenFireBase();
+        $(window).scroll(function(){
+            $('#load-more').each(function(){
+                if (loadingProjects){
+                    return;
+                }
+                var loadMoreElem = $('#load-more');
+                var loadMoreOffset = loadMoreElem.offset().top;
+                var windowHeight = $(window).height();
+                var scrollTop = $(window).scrollTop();
+                if( (loadMoreOffset) - ( scrollTop + windowHeight ) < 0 ){
+                    loadMore();
+                }
+            });
+
+        });
+    });
+
+    //FUNCIÓN PARA MOSTRAR TANTOS PROYECTOS COMO QUEPAN EN LA PANTALLA
+    function loadMore() {
+        console.log("entra click");
+        currentPage++;
+        $("#display-projects").cargaRepositoriosGithub();
+    };
+
+
--- index.md
+++ index.md
@@ -5,12 +5,75 @@ tagline: Proyectos GitHub Del Grupo de Sistemas Inteligentes de la UPM
 ---
 {% include JB/setup %}
 
-Here's a sample "posts list".
-
-<ul class="posts">
-  {% for post in site.posts %}
-    <li><span>{{ post.date | date_to_string }}</span> &raquo; <a href="{{ BASE_PATH }}{{ post.url }}">{{ post.title }}</a></li>
-  {% endfor %}
-</ul>
-
-
+<div class="row">
+      <div class="col-md-6">
+            <div class="jumbotron main-box" data-toggle="collapse" data-target="#divmain1">
+        		  <h2>Agentes y Simulación Social</h2>
+        			<div class="collapse" id="divmain1">
+        				<p>Dentro de este área hacemos uso de agentes inteligentes para la gestión de redes y servicios; aplicaciones educacionales; asistencia personal; y, simulación social.</p>
+                <div class="btn-group btn-group-justified">
+                    <a href="{{ BASE_PATH }}/Categorias.html" class="btn btn-primary btn-lg">Ver más</a>
+                </div>
+        			</div>
+      		</div>
+      </div>
+      <div class="col-md-6">
+            <div class="jumbotron main-box" data-toggle="collapse" data-target="#divmain2">
+        		  <h2>NLP y Análisis de Sentimientos</h2>
+	        		<div class="collapse" id="divmain2">
+	    		        <p>En este área trabajamos en el análisis de sentimientos y emociones en redes sociales, en la búsqueda semántica y en interfaces NLP.</p>
+                  <div class="btn-group btn-group-justified">
+                    <a href="{{ BASE_PATH }}/Categorias.html" class="btn btn-primary btn-lg">Ver más</a>
+                  </div>
+	        		</div>
+      		</div>
+      </div>
+</div>
+<div class="row">
+      <div class="col-md-6">
+            <div class="jumbotron main-box" data-toggle="collapse" data-target="#divmain3">
+        		  <h2>Big Data y Aprendizaje Automático</h2>
+        			<div class="collapse" id="divmain3">
+	    		        <p>Este área engloba el diagnóstico y clasificación distribuidos, la búsqueda semántica a gran escala, el análisis de redes sociales, y el análisis de emociones.</p>
+                  <div class="btn-group btn-group-justified">
+                    <a href="{{ BASE_PATH }}/Categorias.html" class="btn btn-primary btn-lg">Ver más</a>
+                  </div>
+	        		</div>
+      		</div>
+      </div>
+      <div class="col-md-6">
+            <div class="jumbotron main-box" data-toggle="collapse" data-target="#divmain4">
+        		<h2>La Web de Datos y Tecnologías Semánticas</h2>
+        		    <div class="collapse" id="divmain4">
+	    		        <p>En el GSI estamos interesados en la aplicación de la tecnología semántica a en la Web Social y la Web de las Cosas</p>
+                  <div class="btn-group btn-group-justified">
+                    <a href="{{ BASE_PATH }}/Categorias.html" class="btn btn-primary btn-lg">Ver más</a>
+                  </div>
+	        		</div>
+      		</div>
+      </div>
+</div>
+<div class="row">
+      <div class="col-md-6">
+            <div class="jumbotron main-box" data-toggle="collapse" data-target="#divmain5">
+        		<h2>Ingeniería Web y de servicios</h2>
+        		    <div class="collapse" id="divmain5">
+	    		        <p>En esta línea, estamos investigando metodologías y tecnologías que faciliten el desarrollo ágil de aplicaciones, especialmente de los servicios web.</p>
+                  <div class="btn-group btn-group-justified">
+                    <a href="{{ BASE_PATH }}/Categorias.html" class="btn btn-primary btn-lg">Ver más</a>
+                  </div>
+	        		</div>
+      		</div>
+      </div>
+      <div class="col-md-6">
+            <div class="jumbotron main-box" data-toggle="collapse" data-target="#divmain6">
+        		<h2>Otros</h2>
+        		    <div class="collapse" id="divmain6">
+	    		        <p class="lead">Algunos de nuestros proyectos no encajan en ninguna de las categorías definidas.</p>
+                  <div class="btn-group btn-group-justified">
+                    <a href="{{ BASE_PATH }}/Categorias.html" class="btn btn-primary btn-lg">Ver más</a>
+                  </div>                  
+	        		</div>
+      		</div>
+      </div>
+</div>
